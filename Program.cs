99c9adefca2bbc88bad9cb01a1af63d07bc3b901
Kevin Carr <kevin143carr@yahoo.com>
2026-01-13 07:49:29 -0500

    Updated copyright and Licensing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,40 @@
-﻿using System;
+﻿/*
+Excel2DBSharp - Spreadsheet to SQL Import Tool
+Author: Kevin Carr
+License: MIT
+Copyright (c) 2026 Kevin Carr
+
+Description:
+Converts Excel and CSV files into SQL INSERT statements using
+optional JSON-based column mappings, defaults, and header offsets.
+
+Repository: https://github.com/kevin143carr/Excel2DBSharp
+
+MIT License
+
+Copyright (c) 2026 Kevin Carr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
